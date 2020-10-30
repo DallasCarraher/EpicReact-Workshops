327562bd692e5695ceed67f07d10bb5882a01005
DallasCarraher <dallas.carraher@gmail.com>
2020-10-30 10:02:48 -0700

    extra credit, using a status

--- react-hooks/src/exercise/06.js
+++ react-hooks/src/exercise/06.js
@@ -11,18 +11,37 @@ import {
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState('idle')
 
   React.useEffect(() => {
-    if (!pokemonName) { return }
-    setPokemon(null)
-    fetchPokemon(pokemonName).then(data => setPokemon(data))
+    if (!pokemonName) {
+      return
+    }
+    setStatus('pending')
+    fetchPokemon(pokemonName)
+      .then(data => {
+        setPokemon(data)
+        setStatus('resolved')
+      })
+      .catch(error => {
+        setError(error)
+        setStatus('rejected')
+      })
   }, [pokemonName])
 
-  if (!pokemonName) {
+  if (status === 'idle') {
     return 'Submit a pokemon'
-  } else if (!pokemon) {
+  } else if (status === 'pending') {
     return <PokemonInfoFallback name={pokemonName} />
-  } else {
+  } else if (status === 'rejected') {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  } else if (status === 'resolved') {
     return <PokemonDataView pokemon={pokemon} />
   }
 }
