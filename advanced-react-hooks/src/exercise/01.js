904b549077d10a0b38667990e152bba7398a02a7
DallasCarraher <dallas.carraher@gmail.com>
2020-11-29 10:54:05 -0800

    exercise 1 extra credit 1

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -3,14 +3,15 @@
 
 import * as React from 'react'
 
-function countReducer(prevCount, newCount) {
-  return newCount
+function countReducer(count, step) {
+  return count + step
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
 
-  const increment = () => setCount(count + step)
+  // const increment = () => setCount(count + step)
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
 
