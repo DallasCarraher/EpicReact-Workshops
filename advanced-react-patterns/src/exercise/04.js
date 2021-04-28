d22cdd97fd028a6acb34972d630137805dd9f610
DallasCarraher <dallas.carraher@gmail.com>
2021-04-27 19:43:32 -0700

    prop collections

--- advanced-react-patterns/src/exercise/04.js
+++ advanced-react-patterns/src/exercise/04.js
@@ -7,11 +7,11 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  return {
+    on,
+    toggle,
+    togglerProps: {'aria-pressed': on, onClick: toggle},
+  }
 }
 
 function App() {
